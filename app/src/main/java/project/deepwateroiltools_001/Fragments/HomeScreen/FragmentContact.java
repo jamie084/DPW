341e25920b500ff5b5e461e3e134e5bfbfb38f27
jmudri <janos.mudri@gmail.com>
2018-03-04 12:49:15 +0000

    clean up

--- app/src/main/java/project/deepwateroiltools_001/Fragments/HomeScreen/FragmentContact.java
+++ app/src/main/java/project/deepwateroiltools_001/Fragments/HomeScreen/FragmentContact.java
@@ -1,30 +1,28 @@
 package project.deepwateroiltools_001.Fragments.HomeScreen;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
+
+
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
+
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import java.util.ArrayList;
-import java.util.List;
-import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+
 
 import project.deepwateroiltools.HTTP.Common;
 import project.deepwateroiltools.HTTP.ProcessListener;
