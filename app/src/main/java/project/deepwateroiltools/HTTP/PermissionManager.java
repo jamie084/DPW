3349531f3153a603caaeeb886d8449f05826103b
janos <janos.mudri@gmail.com>
2018-03-06 11:48:12 +0000

    Extended with CallPhone function

--- app/src/main/java/project/deepwateroiltools/HTTP/PermissionManager.java
+++ app/src/main/java/project/deepwateroiltools/HTTP/PermissionManager.java
@@ -1,33 +1,54 @@
 package project.deepwateroiltools.HTTP;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.content.PermissionChecker;
 
 /**
  * Created by janos on 04/03/2018.
+ *
+ * Class designed to handle the permission request and ask the user for permissions
  */
 
-public class PermissionManager {
-    private final int MY_PERMISSIONS_REQUEST_STORAGE = 1;
+public class PermissionManager extends Activity {
+    private final int MY_PERMISSIONS_REQUEST = 1;
+    private final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
     Activity activity;
 
     public PermissionManager(Activity activity){
         this.activity = activity;
     }
+
+    //Permission to read & write to external storage (ie SD card)
     public boolean WriteAndReadExternal(){
         if ( (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)  || (activity.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)){
             activity.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_STORAGE);
+                    MY_PERMISSIONS_REQUEST);
             activity.requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_STORAGE);
+                    MY_PERMISSIONS_REQUEST);
             return false;
 
         } else {
-            //   Intent callIntent = new Intent(Intent.ACTION_CALL);
-            //  callIntent.setData(Uri.parse(Common.SUPPORT_PHONE_NUMBER));
-            //startActivity(callIntent);
-            return true; //createPDF.createandDisplayPdf();
+            return true;
         }
     }
+
+    //Permission request to let the application start calls
+    public boolean CallPhone(){
+        if (activity.checkSelfPermission(Manifest.permission.CALL_PHONE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+            return false;
+    }
+    else{
+            return true;
+        }
+    }
+
+
 }
