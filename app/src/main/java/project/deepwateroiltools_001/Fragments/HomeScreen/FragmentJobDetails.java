f25ece47bdbc4d9254f46b29dbcfff03c238e3ea
janos <janos.mudri@gmail.com>
2018-03-11 17:00:02 +0000

    Load btn added OnClick listener changed to switch statement

--- app/src/main/java/project/deepwateroiltools_001/Fragments/HomeScreen/FragmentJobDetails.java
+++ app/src/main/java/project/deepwateroiltools_001/Fragments/HomeScreen/FragmentJobDetails.java
@@ -1,6 +1,7 @@
 package project.deepwateroiltools_001.Fragments.HomeScreen;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -10,10 +11,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import project.deepwateroiltools.HTTP.PermissionManager;
+import project.deepwateroiltools_001.SeaCure;
+import project.helpers.PermissionManager;
 import project.deepwateroiltools_001.HomeScreen;
 import project.deepwateroiltools_001.R;
 import project.dto.SeaCure_job;
@@ -26,7 +30,7 @@ import project.iTextPDF.CreatePDF;
 
 public class FragmentJobDetails extends Fragment implements View.OnClickListener{
     View view;
-    Button btn_back, btn_exportPDF;
+    Button btn_back, btn_exportPDF, btn_load;
     SeaCure_job seaCure_job;
     User user;
     TextView crossfade_text;
@@ -46,8 +50,11 @@ public class FragmentJobDetails extends Fragment implements View.OnClickListener
         Log.d("WelcomScreenFragemtn","LOADED");
         btn_back = (Button) view.findViewById(R.id.btn_back);
         btn_exportPDF = (Button) view.findViewById(R.id.btn_exportPDF);
+        btn_load = (Button) view.findViewById(R.id.btn_load);
+
         btn_back.setOnClickListener(this);
         btn_exportPDF.setOnClickListener(this);
+        btn_load.setOnClickListener(this);
 
         crossfade_text = (TextView) view.findViewById(R.id.crossfade_text);
         crossfade_text.setText(seaCure_job.toString());
@@ -63,17 +70,40 @@ public class FragmentJobDetails extends Fragment implements View.OnClickListener
     }
     @Override
     public void onClick(View v) {
-        if (v == btn_back){
-            getFragmentManager().popBackStackImmediate();
-        }
-        else if (v == btn_exportPDF){
-            permissionManager = new PermissionManager(getActivity());
-            if (permissionManager.WriteAndReadExternal()){
-                SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy.HH:mm");
-                String date = formatter.format(new Date(seaCure_job.getStartDate()));
-                createPDF = new CreatePDF(getContext(), seaCure_job.toString(), user.getUserInfo().getFullName() + "_" + date);
-                createPDF.createandDisplayPdf();
-            }
+//        if (v == btn_back){
+//
+//        }
+//        else if (v == btn_exportPDF){
+//
+//        }
+
+        switch(v.getId()) {
+            case R.id.btn_back:
+                getFragmentManager().popBackStackImmediate();
+                break;
+
+            case R.id.btn_exportPDF:
+                permissionManager = new PermissionManager(getActivity());
+                if (permissionManager.WriteAndReadExternal()){
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy.HH:mm");
+                    String date = formatter.format(new Date(seaCure_job.getStartDate()));
+                    createPDF = new CreatePDF(getContext(), seaCure_job.toString(), user.getUserInfo().getFullName() + "_" + date);
+                    createPDF.createandDisplayPdf();
+                }
+                break;
+
+            case R.id.btn_load:
+                Intent intent = new Intent(getContext(), SeaCure.class);
+                intent.putExtra("loadedSeaCureJob", (new Gson().toJson(seaCure_job)));
+                intent.putExtra("user", (new Gson().toJson(user)));
+
+
+
+           //     User user = users.get(0);
+             //   Intent intentWelcome = new Intent(getApplicationContext(), HomeScreen.class);
+               // intentWelcome.putExtra("user", (new Gson()).toJson(user));
+                startActivity(intent);
+                break;
         }
     }
 }
