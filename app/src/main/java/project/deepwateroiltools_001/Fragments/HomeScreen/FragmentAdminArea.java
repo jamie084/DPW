3db228e01a94a04dd5e7586b9e0e6d031a5cb8f9
janos <janos.mudri@gmail.com>
2018-03-18 18:45:58 +0000

    Listview added with onclick listener; ddl for user and serial number added

--- app/src/main/java/project/deepwateroiltools_001/Fragments/HomeScreen/FragmentAdminArea.java
+++ app/src/main/java/project/deepwateroiltools_001/Fragments/HomeScreen/FragmentAdminArea.java
@@ -2,6 +2,8 @@
 package project.deepwateroiltools_001.Fragments.HomeScreen;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -9,10 +11,14 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,27 +26,38 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import project.deepwateroiltools.HTTP.Common;
 import project.deepwateroiltools.HTTP.ProcessListener;
 import project.deepwateroiltools.HTTP.RunDBQueryWithDialog;
 import project.deepwateroiltools_001.HomeScreen;
 import project.deepwateroiltools_001.R;
+import project.deepwateroiltools_001.SeaCure;
 import project.dto.DotSerail;
+import project.dto.SeaCure_job;
 import project.dto.user.User;
 
 /**
  * Created by janos on 06/02/2018.
  */
 
-public class FragmentAdminArea extends Fragment implements View.OnClickListener {
+public class FragmentAdminArea extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     private View view;
     private Button btn_adminTest;
     private RunDBQueryWithDialog runDBQuery;
     private List<User> users;
     private DotSerail dotSerail;
+    private ListView mainListView;
+    private List<SeaCure_job> jobs;
+    private User user;
+
+    int check = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,19 +72,15 @@ public class FragmentAdminArea extends Fragment implements View.OnClickListener 
 
         HomeScreen homeScreen = (HomeScreen)getActivity();
         dotSerail = homeScreen.getDotSerail();
-        Log.d("dotSerial", String.valueOf(dotSerail.getSeaCureToolSerials().get(0)));
-
-        List<String> elements = new ArrayList<>();
-        for (Integer i:dotSerail.getSeaCureToolSerials()) {
-            elements.add(String.valueOf(i));
-        }
-        createDdl(elements, "SeaCure Tool Serials: ");
-
-        //url to seacure procedure db entries
-        String url = Common.getUrlUser() + Common.getApiKey();
+        user = homeScreen.getUser();
 
 
 
+        // Find the ListView resource.
+        mainListView = (ListView)view. findViewById( R.id.mainListView );
+
+        //url to get all the users, but only including the id and the name fields
+        String url = Common.getUrlUser() + Common.getApiKey() + "&f={\"_id\":1,\"userInfo.firstName\":1,\"userInfo.secondName\":1}";;
 
         //run an async method to populate the procedureslide list
         runDBQuery = new RunDBQueryWithDialog(getActivity(), url, "Loading...");
@@ -87,7 +100,13 @@ public class FragmentAdminArea extends Fragment implements View.OnClickListener 
                         elements.add(user.getUserInfo().getFullName());
                     }
 
-                    createDdl(elements, "Users");
+                    createDdl(elements, "Users", 0);
+
+                    elements = new ArrayList<>();
+                    for (Integer i:dotSerail.getSeaCureToolSerials()) {
+                        elements.add(String.valueOf(i));
+                    }
+                    createDdl(elements, "SeaCure Tool Serials: ", 1);
 
                 }
                 else{
@@ -102,7 +121,7 @@ public class FragmentAdminArea extends Fragment implements View.OnClickListener 
 
     }
 
-    public void createDdl(List<String> elements, String title){
+    public void createDdl(List<String> elements, String title, Integer id){
         LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.linearLayout);
         final float scale = getContext().getResources().getDisplayMetrics().density;
         int pixels = (int) (320 * scale + 0.5f);
@@ -144,8 +163,9 @@ public class FragmentAdminArea extends Fragment implements View.OnClickListener 
         s.setSelection(adapter.getCount());
         s.setMinimumWidth(pixels/2);
         s.setGravity(Gravity.CENTER_HORIZONTAL);
+        s.setOnItemSelectedListener(this);
 
-        //s.setId(i);
+        s.setId(id);
 
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
@@ -174,5 +194,108 @@ public class FragmentAdminArea extends Fragment implements View.OnClickListener 
         if (v == btn_adminTest){
             Toast.makeText(getActivity(), "Admin btn Click", Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(++check > 2) {
+            String url = "";
+            Toast.makeText(getContext(), String.valueOf(parent.getId()), Toast.LENGTH_SHORT).show();
+            //url to seacure procedure db entries
+            for (User user : users) {
+
+            }
+            switch (parent.getId()) {
+                case 0:
+                    if (position < users.size()) {
+                        url = Common.getUrlSeaCureJobs() + Common.getApiKey() + "&q={\"_user_id\":\"" + users.get(position).get_id().getOid() + "\"}";
+                    }
+                    break;
+                case 1:
+                    if (position < dotSerail.getSeaCureToolSerials().size()) {
+                        url = Common.getUrlSeaCureJobs() + Common.getApiKey() + "&q={\"tool_serial\":" + dotSerail.getSeaCureToolSerials().get(position) + "}";
+                    }
+                    break;
+            }
+
+            //run an async method to populate the procedureslide list
+            runDBQuery = new RunDBQueryWithDialog(getActivity(), url, "Loading...");
+            runDBQuery.setProcessListener(new ProcessListener() {
+                @Override
+                public void ProcessingIsDone(String result) {
+                    Gson gson = new Gson();
+                    Type listType = new TypeToken<List<SeaCure_job>>() {
+                    }.getType();
+
+                    List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+                    jobs = gson.fromJson(result, listType);
+
+                    if (!jobs.isEmpty()) {
+                        Log.d("users", result);
+                        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                        for (int i = 0; i < jobs.size(); i++) {
+                            Map<String, String> datum = new HashMap<String, String>(2);
+                            datum.put("title", "Start: " + formatter.format(new Date(jobs.get(i).getStartDate())) + (jobs.get(i).isFinished() ?  " Completed" : " Not-Completed"));
+                            datum.put("date", "Operator: " + jobs.get(i).getClientOperator() + "\nCompany: " ); //TODO get the user from user list+  user.getUserInfo().getCompany());
+                            data.add(datum);
+                        }
+
+
+
+                    } else {
+                        Toast.makeText(getActivity(), "No job found with the selected criteria.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    SimpleAdapter adapter = new SimpleAdapter (getActivity(), data,
+                            android.R.layout.simple_list_item_2,
+                            new String[] {"title", "date"},
+                            new int[] {android.R.id.text1,
+                                    android.R.id.text2,
+                            });
+
+                    mainListView.setAdapter(adapter);
+                    //onclick listener on the jobs to load the fragment with the job detials
+                    mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            check = 0;
+                            FragmentJobDetails fragmentJobDetails = new FragmentJobDetails();
+                            fragmentJobDetails.setSeaCure_job(jobs.get(position));
+
+                            loadFragment(fragmentJobDetails);
+                        }
+                    });
+
+                }
+            });
+
+                runDBQuery.execute();
+
+
+
+            if (id == 0) {
+                if (position < users.size()) {
+                    Toast.makeText(parent.getContext(),
+                            "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString() + " test: " + users.get(position).get_id().getOid(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.crossfade_content, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
